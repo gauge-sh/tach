eef308d9382ced8794d6dadb2a67ff3558ca697f
Caelean <caeleanb@gmail.com>
2024-01-31 13:20:50 -0800

    more tests

--- tests/test_guard.py
+++ tests/test_guard.py
@@ -3,7 +3,7 @@ from modguard.base import guard
 
 
 
-@guard(deny=['func_two'])
+@guard(deny=['func_two'], allow=['func_three'])
 def func_one():
     pass
 
@@ -11,8 +11,13 @@ def func_one():
 def func_two():
     func_one()
 
-
+def func_three():
+    func_one()
 
 def test_func_one_not_allowed_in_func_two():
     with pytest.raises(RuntimeError):
         func_two()
+
+
+def test_func_one_allowed_in_func_three():
+    func_three()
