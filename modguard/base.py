56df7e6e197f0dcc3e1f540ccdd2cc93c8fcfac3
Caelean <caeleanb@gmail.com>
2024-02-01 10:03:15 -0800

    comments

--- modguard/base.py
+++ modguard/base.py
@@ -7,7 +7,9 @@ def guard(allow: Iterable[str] = [], deny: Iterable[str] = [], ):
             current_frame = inspect.currentframe()
             previous_frame = current_frame.f_back
             module = inspect.getmodule(previous_frame)
+            # Can use module name
             module_name = module.__name__
+            # Also have function name
             function_name = previous_frame.f_code.co_name
             full_name = '.'.join((module_name, function_name))
             print(full_name)
