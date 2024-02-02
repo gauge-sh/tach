7b349121f1a8e965dc09f535593af2ce041a7f7f
Caelean <caeleanb@gmail.com>
2024-02-02 14:02:55 -0800

    formatting

--- modguard/cli.py
+++ modguard/cli.py
@@ -1,2 +1,15 @@
+import argparse
+from modguard.check import check
+
+parser = argparse.ArgumentParser(
+    prog="modguard",
+    description="Verify module boundaries are correctly implemented.",
+    epilog="Make sure modguard is run from the root of your repo that a directory is being specified. For example, `modguard .`",
+)
+
+parser.add_argument("path")
+
+
 def execute():
-    print('eggxecute')
+    args = parser.parse_args()
+    check(args.path)
