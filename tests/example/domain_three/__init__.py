59a89eee9a7ec745b98dfb83546867025ee7e25d
Evan Doyle <evanmdoyle@gmail.com>
2024-02-05 14:15:50 -0800

    Adjust test imports in example dir

--- tests/example/domain_three/__init__.py
+++ tests/example/domain_three/__init__.py
@@ -1,4 +1,5 @@
 from modguard import Boundary
+from .api import public_for_domain_two
 from ..domain_one.interface import domain_one_interface
 
 Boundary()
--- tests/example/domain_two/__init__.py
+++ tests/example/domain_two/__init__.py
@@ -1,2 +1,2 @@
 from ..domain_one.interface import domain_one_interface
-from ..domain_three.api import public_for_domain_two
+from ..domain_three import api
