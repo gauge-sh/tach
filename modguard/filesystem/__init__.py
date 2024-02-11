ef41ad75fc987c2479e62f7c89013ed5a2062d26
Evan Doyle <evanmdoyle@gmail.com>
2024-02-10 22:38:57 -0800

    Explicit imports at filesystem init

--- modguard/filesystem/__init__.py
+++ modguard/filesystem/__init__.py
@@ -1,5 +1,16 @@
 from modguard import Boundary, public
-from .service import *
+from .service import (
+    get_cwd,
+    chdir,
+    canonical,
+    read_file,
+    write_file,
+    parse_ast,
+    walk_pyfiles,
+    walk_pypackages,
+    file_to_module_path,
+    module_to_file_path,
+)
 
 Boundary()
 
