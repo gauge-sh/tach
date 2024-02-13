705ec6e8813283a6688c75b2402c882261c33c27
caelean <caeleanb@gmail.com>
2024-02-12 22:29:18 -0800

    pyright passing

--- modguard/show.py
+++ modguard/show.py
@@ -1,27 +1,30 @@
 import yaml
 from modguard.core.boundary import BoundaryTrie
+from typing import Any, Dict
 
+# This type hint only works on more recent versions
+# result_dict: TypeAlias = dict[str, str | bool | 'result_dict']
 
-def boundary_trie_to_dict(boundary_trie):
-    result = {}
+def boundary_trie_to_dict(boundary_trie: BoundaryTrie) -> Dict[str, Any]:
+    result: Dict[str, Any] = dict()
     for node in boundary_trie:
         path = node.full_path
         if path == "":
             path = "root"
         sections = path.split(".")
-        pointer = result
+        pointer: Dict[str, Any] = result
         for section in sections:
             if section not in pointer:
-                pointer[section] = {}
+                pointer[section] = dict()
             pointer = pointer[section]
         pointer["is_boundary"] = True
 
         for member in node.public_members.keys():
-            pointer = result
+            pointer: Dict[str, Any] = result
             sections = member.split(".")
             for section in sections:
                 if section not in pointer:
-                    pointer[section] = {}
+                    pointer[section] = dict()
                 pointer = pointer[section]
             pointer["is_public"] = True
 
