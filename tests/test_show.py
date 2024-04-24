6ff3d902ce0305bbd6844864422cd9bf1db8afc5
caelean <caeleanb@gmail.com>
2024-04-24 14:07:46 -0700

    passing test, dev-reqs

--- tests/test_show.py
+++ tests/test_show.py
@@ -2,6 +2,7 @@ import pytest
 
 # from modguard.show import show
 from modguard.core import BoundaryTrie, PublicMember, BoundaryNode
+from modguard.show import show
 
 
 @pytest.fixture
@@ -52,21 +53,20 @@ def boundary_trie():
     )
 
 
-#
-# def test_show(boundary_trie):
-#     assert (
-#         show(boundary_trie)[0]
-#         == """domain_four:
-#   is_boundary: true
-#   public_api:
-#     is_public: true
-# domain_one:
-#   is_boundary: true
-# domain_three:
-#   is_boundary: true
-# domain_two:
-#   is_boundary: true
-#   subdomain:
-#     is_boundary: true
-# """
-#     )
+def test_show(boundary_trie):
+    assert (
+        show(boundary_trie)[0]
+        == """domain_one:
+  is_boundary: true
+domain_two:
+  is_boundary: true
+  subdomain:
+    is_boundary: true
+domain_three:
+  is_boundary: true
+domain_four:
+  is_boundary: true
+  public_api:
+    is_public: true
+"""
+    )
