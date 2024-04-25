18f44e5f58e3ee9a43f2595120ac7028e06b15c8
Evan Doyle <evanmdoyle@gmail.com>
2024-04-25 15:30:00 -0700

    boundary -> module in test_module_trie

--- tests/test_module_trie.py
+++ tests/test_module_trie.py
@@ -9,7 +9,7 @@ def test_config() -> ModuleConfig:
 
 
 @pytest.fixture
-def boundary_trie() -> ModuleTrie:
+def module_trie() -> ModuleTrie:
     return ModuleTrie(
         root=ModuleNode(
             is_end_of_path=False,
@@ -57,8 +57,8 @@ def test_iterate_over_empty_trie():
     assert list(ModuleTrie()) == []
 
 
-def test_iterate_over_populated_trie(boundary_trie):
-    assert set((node.full_path for node in boundary_trie)) == {
+def test_iterate_over_populated_trie(module_trie):
+    assert set((node.full_path for node in module_trie)) == {
         "domain_one",
         "domain_one.subdomain",
         "domain_two",
@@ -67,8 +67,8 @@ def test_iterate_over_populated_trie(boundary_trie):
     }
 
 
-def test_get_nonexistent_path(boundary_trie):
-    assert boundary_trie.get("fakepath") is None
+def test_get_nonexistent_path(module_trie):
+    assert module_trie.get("fakepath") is None
 
 
 def test_get_nonexistent_empty_path():
@@ -76,8 +76,8 @@ def test_get_nonexistent_empty_path():
     assert trie.get("") is None
 
 
-def test_get_actual_path(boundary_trie):
-    assert boundary_trie.get("domain_one") is not None
+def test_get_actual_path(module_trie):
+    assert module_trie.get("domain_one") is not None
 
 
 def test_insert_empty_path(test_config):
@@ -98,16 +98,16 @@ def test_insert_multi_level_path(test_config):
     assert set((node.full_path for node in trie)) == {"domain.subdomain"}
 
 
-def test_find_nearest_at_root(boundary_trie):
-    boundary = boundary_trie.find_nearest("other_domain")
-    assert boundary is None
+def test_find_nearest_at_root(module_trie):
+    module = module_trie.find_nearest("other_domain")
+    assert module is None
 
 
-def test_find_nearest_in_single_domain(boundary_trie):
-    boundary = boundary_trie.find_nearest("domain_one.thing")
-    assert boundary.full_path == "domain_one"
+def test_find_nearest_in_single_domain(module_trie):
+    module = module_trie.find_nearest("domain_one.thing")
+    assert module.full_path == "domain_one"
 
 
-def test_find_nearest_in_nested_domain(boundary_trie):
-    boundary = boundary_trie.find_nearest("domain_two.subdomain.thing")
-    assert boundary.full_path == "domain_two.subdomain"
+def test_find_nearest_in_nested_domain(module_trie):
+    module = module_trie.find_nearest("domain_two.subdomain.thing")
+    assert module.full_path == "domain_two.subdomain"
