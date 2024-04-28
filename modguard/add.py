b2962010d01875ef134f2619061b52c49d5e19f9
caelean <caeleanb@gmail.com>
2024-04-27 18:00:55 -0700

    modguard errors

--- modguard/add.py
+++ modguard/add.py
@@ -6,7 +6,7 @@ import yaml
 from modguard import filesystem as fs
 from modguard.check import check
 from modguard.constants import CONFIG_FILE_NAME
-from modguard.core import ProjectConfig, ScopeDependencyRules
+from modguard.core import ScopeDependencyRules
 from modguard.parsing import parse_project_config
 
 
@@ -24,14 +24,14 @@ def update_project_config(root: str, tags: set[str]):
             if error.is_tag_error:
                 invalid_tags = set(error.invalid_tags)
                 if error.source_tag in tags or invalid_tags & tags:
-                existing_dependencies = set(
-                    project_config.constraints.get(
-                        error.source_tag, ScopeDependencyRules(depends_on=[])
-                    ).depends_on
-                )
-                project_config.constraints[error.source_tag] = ScopeDependencyRules(
-                    depends_on=list(existing_dependencies | (invalid_tags & tags))
-                )
+                    existing_dependencies = set(
+                        project_config.constraints.get(
+                            error.source_tag, ScopeDependencyRules(depends_on=[])
+                        ).depends_on
+                    )
+                    project_config.constraints[error.source_tag] = ScopeDependencyRules(
+                        depends_on=list(existing_dependencies | (invalid_tags & tags))
+                    )
 
         modguard_yml_path = os.path.join(root, f"{CONFIG_FILE_NAME}.yml")
         modguard_yml_content = yaml.dump(project_config.model_dump())
