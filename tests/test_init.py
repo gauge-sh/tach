1720bd4f9aa6a7a634c03d1ed5b85d4e862f9089
Evan Doyle <evanmdoyle@gmail.com>
2024-05-16 22:56:00 -0700

    Remove outdated test, rename outdated test

--- tests/test_init.py
+++ tests/test_init.py
@@ -27,45 +27,6 @@ def test_root():
     shutil.rmtree(test_root)
 
 
-def test_init_project_with_valid_root(test_root):
-    # Create some mock files and directories for testing
-    test_dirs = [
-        "package1",
-        "package2",
-        "package3",
-        "package4/subpackage",
-        "package5/subpackage",
-        "package6/subpackage",
-    ]
-    for d in test_dirs:
-        os.makedirs(os.path.join(test_root, d))
-        with open(os.path.join(test_root, d, "__init__.py"), "w") as f:
-            f.write("# Mock __init__.py file")
-
-    # Create some mock Python files with imports and member names
-    file_contents = {
-        "package1/__init__.py": "from package6.subpackage.module6 import x",
-        "package2/__init__.py": "",
-        "package1/module1.py": "class Package1Class:\n    pass\n",
-        "package2/module2.py": "def package_2_func():\n    pass\n",
-        "package3/__init__.py": "from package1.module1 import Package1Class\nfrom package2.module2 import package_2_func\n",
-        "package3/module3.py": "",
-        "package4/subpackage/__init__.py": "",
-        "package5/subpackage/__init__.py": "import package3.module3",
-        "package6/subpackage/__init__.py": "",
-        "package6/subpackage/module6.py": "x = 3\n",
-    }
-
-    for file_path, content in file_contents.items():
-        with open(os.path.join(test_root, file_path), "w") as f:
-            f.write(content)
-
-    # Call init_project with the test root
-    init_project_from_root(test_root)
-
-    # TODO: test new behavior
-
-
 def test_init_project_with_invalid_root():
     with pytest.raises(errors.TachSetupError):
         init_project("nonexistent_directory")
