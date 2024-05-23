8459bd6d196ad52bdc1cc58418bb215550f29770
caelean <caeleanb@gmail.com>
2024-05-23 11:59:02 -0700

    updates to logger

--- tach/logging/logger.py
+++ tach/logging/logger.py
@@ -6,19 +6,37 @@ import os
 import threading
 
 import requests
+from pydantic import BaseModel
+
+LOGGING_URL = ""
+
+
+class LogDataModel(BaseModel):
+    function: str
+    parameters: dict
 
 
 def send_log_entry(url, record: logging.LogRecord, entry: str) -> None:
+    is_ci = "CI" in os.environ
+    data = record.data if hasattr(record, "data") else None
+
+    log_data = {
+        "log": entry,
+        "data": data,
+        "level": record.levelname,
+        "timestamp": record.created,
+        "is_ci": is_ci,
+    }
     try:
         response = requests.post(
             url,
-            data=json.dumps({"log": entry}),
+            data=json.dumps(log_data),
             headers={"Content-Type": "application/json"},
         )
         response.raise_for_status()
     except Exception:
         # Optionally, handle exceptions (e.g., logging to a file)
-        print(record, entry)
+        print(log_data)
         # print(f"Failed to send log entry: {entry}: {e}")
 
 
@@ -37,8 +55,7 @@ class RemoteLoggingHandler(logging.Handler):
 
 logger = logging.getLogger("tach")
 logger.setLevel(logging.INFO)
-url = "https://your-logging-server.com/logs"
-remote_handler = RemoteLoggingHandler(url)
+remote_handler = RemoteLoggingHandler(LOGGING_URL)
 
 # Check if remote logging is enabled
 REMOTE_LOGGING = os.getenv("REMOTE_LOGGING", "true").lower() == "true"
