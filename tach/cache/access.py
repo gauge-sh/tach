c9bb5f0185d06fe2c43f12940983f69390b3fc1b
caelean <caeleanb@gmail.com>
2024-05-23 20:45:59 -0700

    os.path.exists -> pathlib[..].exists()

--- tach/cache/access.py
+++ tach/cache/access.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import os
 import uuid
 
 from tach.cache.setup import resolve_dot_tach, resolve_project_dir
@@ -8,7 +7,7 @@ from tach.cache.setup import resolve_dot_tach, resolve_project_dir
 
 def get_uid() -> uuid.UUID:
     project_root = resolve_project_dir()
-    if not os.path.exists(project_root / ".tach" / "tach.info"):
+    if not (project_root / ".tach" / "tach.info").exists():
         resolve_dot_tach()
     with open(project_root / ".tach" / "tach.info") as f:
         uid = uuid.UUID(f.read().strip())
