e37222e78f2302822f6c39be6c05629ca6c5a702
caelean <caeleanb@gmail.com>
2024-05-23 20:22:22 -0700

    os -> pathlib

--- tach/cache/setup.py
+++ tach/cache/setup.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
-import os
 import uuid
 from pathlib import Path
 
 
 def resolve_project_dir() -> Path:
-    current_dir = os.getcwd()
-    tach_path = Path(os.path.join(current_dir, "tach.yml"))
+    current_dir = Path.cwd()
+    tach_path = current_dir / "tach.yml"
     if not tach_path.exists():
         raise FileNotFoundError()
     return Path(current_dir)
@@ -16,20 +15,18 @@ def resolve_project_dir() -> Path:
 def resolve_dot_tach() -> Path:
     project_dir = resolve_project_dir()
 
-    def _create(path: str, is_file: bool = False, file_content: str = "") -> None:
-        if not os.path.exists(path):
+    def _create(path: Path, is_file: bool = False, file_content: str = "") -> None:
+        if not path.exists():
             if is_file:
-                with open(path, "w") as f:
-                    f.write(file_content.strip())
+                path.write_text(file_content.strip())
             else:
-                os.makedirs(path)
+                path.mkdir()
 
     # Create .bridge
-    tach_path = os.path.join(project_dir, ".tach")
+    tach_path = project_dir / ".tach"
     _create(tach_path)
     # Create info
-
-    info_path = os.path.join(tach_path, "tach.info")
+    info_path = tach_path / "tach.info"
     _create(info_path, is_file=True, file_content=str(uuid.uuid4()))
     # Create .gitignore
     gitignore_content = """
@@ -38,6 +35,6 @@ def resolve_dot_tach() -> Path:
 # gitignore all content, including this .gitignore
 *
     """
-    gitignore_path = os.path.join(tach_path, ".gitignore")
+    gitignore_path = tach_path / ".gitignore"
     _create(gitignore_path, is_file=True, file_content=gitignore_content)
     return Path(tach_path)
