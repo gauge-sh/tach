a9b74d6f50a36e9cb5d425576f977341fcbf5c04
Evan Doyle <evanmdoyle@gmail.com>
2024-05-27 12:56:55 -0700

    Set up extremely basic benchmark

--- benches/get_project_imports.rs
+++ benches/get_project_imports.rs
@@ -1,36 +1,20 @@
 #[macro_use]
 extern crate criterion;
-use criterion::{black_box, BenchmarkId, Criterion};
+
+use criterion::Criterion;
 
 use pprof::criterion::{Output, PProfProfiler};
-
-// Thanks to the example provided by @jebbow in his article
-// https://www.jibbow.com/posts/criterion-flamegraphs/
-
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 | 1 => 1,
-        n => fibonacci(n - 1) + fibonacci(n - 2),
-    }
-}
+use tach::imports::get_project_imports;
 
 fn bench(c: &mut Criterion) {
-    c.bench_function("Fibonacci", |b| b.iter(|| fibonacci(black_box(20))));
-}
-
-fn bench_group(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Fibonacci Sizes");
-
-    for s in &[1, 10, 100, 1000] {
-        group.bench_with_input(BenchmarkId::from_parameter(s), s, |b, s| {
-            b.iter(|| fibonacci(black_box(*s)))
-        });
-    }
+    c.bench_function("get_project_imports", |b| {
+        b.iter(|| get_project_imports(String::from("python"), String::from("tach/cli.py"), true))
+    });
 }
 
 criterion_group! {
     name = benches;
     config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
-    targets = bench, bench_group
+    targets = bench
 }
 criterion_main!(benches);
