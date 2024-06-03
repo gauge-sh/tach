9ee248881bce563fe39797135154e94272474907
Evan Doyle <evanmdoyle@gmail.com>
2024-06-03 11:23:50 -0700

    Report error when duplicate modules found in config

--- python/tach/parsing/modules.py
+++ python/tach/parsing/modules.py
@@ -9,7 +9,23 @@ if TYPE_CHECKING:
     from tach.core import ModuleConfig
 
 
+def find_duplicate_modules(modules: list[ModuleConfig]) -> list[str]:
+    duplicate_module_paths: list[str] = []
+    seen: set[str] = set()
+    for module in modules:
+        if module.path in seen:
+            duplicate_module_paths.append(module.path)
+            continue
+        seen.add(module.path)
+    return duplicate_module_paths
+
+
 def build_module_tree(modules: list[ModuleConfig]) -> ModuleTree:
+    duplicate_modules = find_duplicate_modules(modules)
+    if duplicate_modules:
+        raise ValueError(
+            f"Failed to build module tree. The following modules were defined more than once: {duplicate_modules}"
+        )
     tree = ModuleTree()
     for module in modules:
         tree.insert(
