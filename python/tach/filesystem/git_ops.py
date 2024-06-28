407751f1b19bca977e42ec418797086f43a67d5b
Evan Doyle <evanmdoyle@gmail.com>
2024-06-28 12:02:36 -0700

    Update get_changed_files to handle file renames, add tests

--- python/tach/filesystem/git_ops.py
+++ python/tach/filesystem/git_ops.py
@@ -19,23 +19,28 @@ def get_changed_files(
 
     try:
         if head:
-            diff: str = repo.git.diff("--name-only", head, base)
+            diff: str = repo.git.diff("--name-status", head, base)
         else:
             # If head is not provided, we can diff against 'base' from the current filesystem
-            diff: str = repo.git.diff("--name-only", base)
+            diff: str = repo.git.diff("--name-status", base)
     except GitCommandError:
         head_display = f"'{head}'" if head else "current filesystem"
         raise TachError(f"Failed to check diff between '{base}' and {head_display}!")
 
-    changed_files = diff.splitlines()
+    diff_lines = diff.splitlines()
+    changed_files: set[str] = set()
+
+    for line in diff_lines:
+        _, *files = line.split("\t")
+        changed_files.update(files)
 
     if not head:
         # If we are using the current filesystem, there may be relevant changes in untracked files
         untracked_files: str = repo.git.ls_files("--others", "--exclude-standard")
-        changed_files.extend(untracked_files.splitlines())
+        changed_files.update(untracked_files.splitlines())
 
     # return list of unique Paths
-    return list(map(Path, set(changed_files)))
+    return list(map(Path, changed_files))
 
 
 __all__ = ["get_changed_files"]
