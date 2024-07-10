313a9d02aa49660ec3ad013184ed2888f400a37d
Tilak Dave <davetilak003@gmail.com>
2024-07-10 20:07:16 +0530

    ruff format

--- python/tach/parsing/modules.py
+++ python/tach/parsing/modules.py
@@ -21,16 +21,22 @@ def find_duplicate_modules(modules: list[ModuleConfig]) -> list[str]:
         seen.add(module.path)
     return duplicate_module_paths
 
+
 def find_modules_with_circular_dependencies(modules: list[ModuleConfig]) -> list[str]:
-    def find_cycle(module: ModuleConfig, visited: set[str], path: list[str], modules: list[ModuleConfig]) -> list[str]:
+    def find_cycle(
+        module: ModuleConfig,
+        visited: set[str],
+        path: list[str],
+        modules: list[ModuleConfig],
+    ) -> list[str]:
         if module.path in visited:
             cycle_start_index = path.index(module.path)
             cycle = path[cycle_start_index:] + [module.path]
             return [f"{cycle[i]}: {cycle[i + 1]}" for i in range(len(cycle) - 1)]
-        
+
         visited.add(module.path)
         path.append(module.path)
-        
+
         for dependency in module.depends_on:
             dep_module = next((mod for mod in modules if mod.path == dependency), None)
             if dep_module:
@@ -40,20 +46,22 @@ def find_modules_with_circular_dependencies(modules: list[ModuleConfig]) -> list
 
         visited.remove(module.path)
         path.pop()
-        
+
         return []
 
     modules_with_cycles = set()
-    
+
     for module in modules:
         cycle = find_cycle(module, set(), [], modules)
         if cycle:
             modules_with_cycles.update(cycle)
-    
+
     return modules_with_cycles
 
 
-def build_module_tree(source_root: Path, modules: list[ModuleConfig], forbid_circular_dependencies: bool) -> ModuleTree:
+def build_module_tree(
+    source_root: Path, modules: list[ModuleConfig], forbid_circular_dependencies: bool
+) -> ModuleTree:
     duplicate_modules = find_duplicate_modules(modules)
     if duplicate_modules:
         raise ValueError(
@@ -64,8 +72,11 @@ def build_module_tree(source_root: Path, modules: list[ModuleConfig], forbid_cir
         modules_with_cycles = find_modules_with_circular_dependencies(modules)
         if modules_with_cycles:
             raise ValueError(
-                "Failed to build module tree. The following modules have circular dependencies\n" + 
-                "\n".join(f"{cycle.split(': ')[0]}\t->\t{cycle.split(': ')[1]}" for cycle in modules_with_cycles)
+                "Failed to build module tree. The following modules have circular dependencies\n"
+                + "\n".join(
+                    f"{cycle.split(': ')[0]}\t->\t{cycle.split(': ')[1]}"
+                    for cycle in modules_with_cycles
+                )
             )
     tree = ModuleTree()
     for module in modules:
