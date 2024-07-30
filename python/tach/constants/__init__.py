2e3eacd24900da1aa2c21bb497c4e04c94bc62de
caelean <caeleanb@gmail.com>
2024-07-29 18:03:36 -0700

    Updated version, yml schema

--- python/tach/constants/__init__.py
+++ python/tach/constants/__init__.py
@@ -6,7 +6,7 @@ CONFIG_FILE_NAME = TOOL_NAME
 PACKAGE_FILE_NAME = "package"
 ROOT_MODULE_SENTINEL_TAG = "<root>"
 TACH_YML_SCHEMA_URL = (
-    "https://raw.githubusercontent.com/gauge-sh/tach/v0.9.0/public/tach-yml-schema.json"
+    "https://raw.githubusercontent.com/gauge-sh/tach/v0.9.1/public/tach-yml-schema.json"
 )
 
 DEFAULT_EXCLUDE_PATHS = ["tests", "docs", "venv", ".*__pycache__", ".*egg-info"]
