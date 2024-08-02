0ad2f09f85bb69c33b4eed50aec31ca042394228
Evan Doyle <evanmdoyle@gmail.com>
2024-08-02 10:35:35 -0700

    Add method for Python < 3.10 to build module mapping

--- python/tach/check_external.py
+++ python/tach/check_external.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import sys
 from dataclasses import dataclass
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from tach.extension import check_external_dependencies, set_excluded_paths
 
@@ -34,14 +34,53 @@ def is_stdlib_module(module: str) -> bool:
         return in_stdlib(module)  # type: ignore
 
 
+def get_installed_modules(dist: Any) -> list[str]:
+    # This method is best-effort, and is only used for Python < 3.10
+    module_names: set[str] = set()
+
+    # Method 1: Check top_level.txt
+    try:
+        top_level = dist.read_text("top_level.txt")
+        if top_level:
+            module_names.update(top_level.strip().split())
+    except Exception:
+        pass
+
+    # Method 2: Parse RECORD file
+    try:
+        record = dist.read_text("RECORD")
+        if record:
+            module_names.update(
+                line.split(",")[0].split("/")[0]
+                for line in record.splitlines()
+                if "/" in line and not line.startswith("__")
+            )
+    except Exception:
+        pass
+
+    # Method 3: Check entry points
+    for ep in dist.entry_points:
+        if "." in ep.value:
+            module_names.add(ep.value.split(":")[0])
+
+    return list(module_names)
+
+
 def get_module_mappings() -> dict[str, list[str]]:
     if sys.version_info >= (3, 10):
         from importlib.metadata import packages_distributions
 
         return packages_distributions()
     else:
-        # TODO
-        return {}
+        if sys.version_info >= (3, 8):  # noqa: UP036
+            from importlib.metadata import distributions
+        else:
+            from importlib_metadata import distributions
+
+        return {
+            dist.metadata["Name"]: get_installed_modules(dist)
+            for dist in distributions()
+        }
 
 
 @dataclass
