1bb016d751ace7e507d6c732d8d0d6acef783f27
caelean <caeleanb@gmail.com>
2024-08-29 08:41:51 -0700

    bump version

--- python/tach/__init__.py
+++ python/tach/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-__version__ = "0.10.3"
+__version__ = "0.10.4"
 
 __all__ = ["__version__"]
--- python/tests/example/valid/domain_two/__init__.py
+++ python/tests/example/valid/domain_two/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from domain_three import x
 from domain_four import ok
+from domain_three import x
 
 __all__ = ["x", "ok"]
