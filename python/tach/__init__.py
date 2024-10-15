c19e3e927eaa31db355909a45a75af33ed082dd2
Evan Doyle <evanmdoyle@gmail.com>
2024-10-15 11:40:20 -0700

    Bump to 0.13.3

--- python/tach/__init__.py
+++ python/tach/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-__version__ = "0.13.2"
+__version__ = "0.13.3"
 
 __all__ = ["__version__"]
--- python/tach/constants/__init__.py
+++ python/tach/constants/__init__.py
@@ -5,7 +5,6 @@ TOOL_NAME = "tach"
 CONFIG_FILE_NAME = TOOL_NAME
 PACKAGE_FILE_NAME = "package"
 ROOT_MODULE_SENTINEL_TAG = "<root>"
-TACH_YML_SCHEMA_URL = "https://raw.githubusercontent.com/gauge-sh/tach/v0.13.2/public/tach-yml-schema.json"
 
 DEFAULT_EXCLUDE_PATHS = ["tests", "docs", ".*__pycache__", ".*egg-info"]
 
@@ -15,6 +14,5 @@ __all__ = [
     "CONFIG_FILE_NAME",
     "PACKAGE_FILE_NAME",
     "ROOT_MODULE_SENTINEL_TAG",
-    "TACH_YML_SCHEMA_URL",
     "DEFAULT_EXCLUDE_PATHS",
 ]
