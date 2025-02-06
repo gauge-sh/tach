4877e835c14d1eb3e64c97f575c723b1d8ed6015
Evan Doyle <evanmdoyle@gmail.com>
2025-02-06 14:02:11 -0800

    Add test for sync behavior on many_features example dir

--- python/tests/test_sync.py
+++ python/tests/test_sync.py
@@ -77,3 +77,51 @@ def test_distributed_config_dir(example_dir, capfd):
             "project.module_two",
             "project.module_one",
         }
+
+
+def test_many_features_example_dir(example_dir, capfd):
+    project_root = example_dir / "many_features"
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_project_root = Path(temp_dir) / "many_features"
+        shutil.copytree(project_root, temp_project_root)
+
+        project_config = parse_project_config(root=temp_project_root)
+        assert project_config is not None
+
+        with pytest.raises(SystemExit) as exc_info:
+            tach_sync(
+                project_root=temp_project_root,
+                project_config=project_config,
+                exclude_paths=project_config.exclude,
+            )
+
+        assert exc_info.value.code == 0
+        captured = capfd.readouterr()
+        assert "✅" in captured.out  # success state
+
+        project_config = parse_project_config(root=temp_project_root)
+        assert project_config is not None
+
+        modules = project_config.filtered_modules([])
+        assert len(modules) == 9
+
+        module2 = next(module for module in modules if module.path == "module2")
+        assert set(map(lambda dep: dep.path, module2.depends_on)) == {"outer_module"}
+
+        module3 = next(module for module in modules if module.path == "module3")
+        assert set(map(lambda dep: dep.path, module3.depends_on)) == {"module1"}
+
+        assert (
+            '"//module1"'
+            in (
+                temp_project_root / "real_src" / "module3" / "tach.domain.toml"
+            ).read_text()
+        )
+
+        assert (
+            '"<domain_root>"'
+            in (
+                temp_project_root / "real_src" / "module3" / "tach.domain.toml"
+            ).read_text()
+        )
